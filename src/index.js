caa4d3fa199315a42df4d3a5953137cdab1fa35f
Rodrigo Rossi <rdrg.rossi@gmail.com>
2020-11-16 13:43:42 -0300

    Change port and return description

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@
 const express = require("express");
 
 // Constants
-const PORT = 8080;
+const PORT = 8081;
 const HOST = "0.0.0.0";
 
 // App
 const app = express();
 app.get("/", (req, res) => {
-  res.send("Respondent service is running");
+  res.send("Question service is running");
 });
 
 app.listen(PORT, HOST);
